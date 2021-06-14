4e6ab73a1208aa3e2a1a1628f2054413b32e11d5
VINSUPP <gangadasu.vinoothna.17cse@bml.edu.in>
2021-06-14 11:39:14 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vinoothna Edit <code>src/App.js</code> and save to reload.
+          Vinoothna likes manga. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
